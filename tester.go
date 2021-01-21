802214c3d536128ceb6ee7cabf5950ec512dd8d5
lucarin91 <lucarin@protonmail.com>
2021-01-21 11:52:33 +0100

    fix: code review #2

--- tester.go
+++ tester.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -14,81 +15,101 @@ func WithRequests(u string, w uint64, n uint64) (Report, error) {
 	if n < w {
 		return Report{}, fmt.Errorf("number of requests cannot be less then worker")
 	}
-	req := make(requestChan, n)
-	res := make(resultChan)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 
-	for i := w; i > 0; i-- {
-		fmt.Println("M: start worker")
-		requestWorker(ctx, u, req, res)
-	}
+	req := numChan(n, w)
+	rep := spawnAndWait(u, w, req)
 
-	var rep Report
-	for i := w; i > 0; i-- {
-		rep.Merge(<-res)
-	}
-	fmt.Println("M: done")
 	return rep, nil
 }
 
 func WithDuration(u string, w uint64, d time.Duration) (Report, error) {
-	res := make(resultChan)
 	ctx, cancel := context.WithTimeout(context.Background(), d)
 	defer cancel()
+	req := cancelChan(ctx, w)
 
-	for i := w; i > 0; i-- {
-		fmt.Println("M: start worker")
-		durationWorker(ctx, u, res)
-	}
+	rep := spawnAndWait(u, w, req)
 
-	var rep Report
-	for i := w; i > 0; i-- {
-		rep.Merge(<-res)
-	}
-	fmt.Println("M: done")
 	return rep, nil
 }
 
-func requestWorker(ctx context.Context, u string, req requestChan, res resultChan) {
-	go func() {
-		var rep Report
-		for {
-			select {
-			case <-ctx.Done():
-			default:
-				fmt.Println("W: done")
-				res <- rep
-				return
-			case req <- struct{}{}:
-				workerRequest(u, &rep)
-			}
-		}
-	}()
-}
-
-func durationWorker(ctx context.Context, u string, res resultChan) {
-	go func() {
-		var rep Report
-		for {
-			select {
-			case <-ctx.Done():
-				fmt.Println("W: done")
-				res <- rep
-				return
-			default:
-				workerRequest(u, &rep)
-			}
-		}
-	}()
-}
-
-func workerRequest(u string, rep *Report) {
-	r, err := http.Get(u)
-	if err != nil {
-		rep.Fail++
-	} else {
-		rep.Success++
-		r.Body.Close()
+func spawnAndWait(u string, w uint64, req requestChan) Report {
+	res := make([]resultChan, 0, w)
+	for i := w; i > 0; i-- {
+		fmt.Println("M: start worker")
+		res = append(res, worker(u, req))
 	}
+	var rep Report
+	for r := range merge(res...) {
+		rep.Merge(r)
+	}
+	fmt.Println("M: done")
+	return rep
+}
+
+func worker(u string, req requestChan) resultChan {
+	res := make(resultChan)
+	go func() {
+		var rep Report
+		for range req {
+			r, err := http.Get(u)
+			if err != nil {
+				rep.Fail++
+			} else {
+				rep.Success++
+				r.Body.Close()
+			}
+		}
+		fmt.Println("W: done")
+		res <- rep
+		close(res)
+	}()
+	return res
+}
+
+func cancelChan(ctx context.Context, cap uint64) requestChan {
+	req := make(requestChan, cap)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				close(req)
+				return
+			default:
+				req <- struct{}{}
+			}
+		}
+	}()
+	return req
+}
+
+func numChan(n, cap uint64) requestChan {
+	req := make(requestChan, cap)
+	go func() {
+		for i := n; i > 0; i-- {
+			req <- struct{}{}
+		}
+		close(req)
+	}()
+	return req
+}
+
+func merge(cs ...resultChan) resultChan {
+	var wg sync.WaitGroup
+	out := make(resultChan)
+
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go func(c resultChan) {
+			for e := range c {
+				out <- e
+			}
+			wg.Done()
+		}(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }

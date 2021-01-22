156977250ffdf14babb61778009eccea8f3dc424
lucarin91 <lucarin@protonmail.com>
2021-01-22 12:15:15 +0100

    fix: code review #6

--- loader_test.go
+++ loader_test.go
@@ -2,36 +2,23 @@ package main
 
 import (
 	"context"
-	"errors"
 	"net/http"
+	"net/http/httptest"
 	"testing"
-	"time"
 )
 
-const host = "localhost:8080"
-const path = "/pZ3hLHse"
-
 func TestRequestNum(t *testing.T) {
+	var count uint64
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		count++
+	}))
+	defer ts.Close()
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	var srv http.Server
-	srv.Addr = host
-	defer srv.Shutdown(ctx)
-	var count uint64
-	http.HandleFunc("/pZ3hLHse", func(w http.ResponseWriter, r *http.Request) {
-		count++
-	})
-	go func() {
-		err := srv.ListenAndServe()
-		if !errors.Is(err, http.ErrServerClosed) {
-			t.Errorf("http server: %v", err)
-		}
-	}()
-	time.Sleep(1 * time.Second)
-
 	count = 0
-	rep, err := WithRequests(ctx, "http://"+host+path, 1, 10)
+	rep, err := WithRequests(ctx, ts.URL, 1, 10)
 	if err != nil {
 		t.Errorf("get %v, want no error", err)
 	}
@@ -40,7 +27,7 @@ func TestRequestNum(t *testing.T) {
 	}
 
 	count = 0
-	rep, err = WithRequests(ctx, "http://"+host+path, 5, 10)
+	rep, err = WithRequests(ctx, ts.URL, 5, 10)
 	if err != nil {
 		t.Errorf("get %v, want no error", err)
 	}
